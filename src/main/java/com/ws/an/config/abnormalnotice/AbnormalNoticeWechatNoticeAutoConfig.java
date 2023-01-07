8933feaad90877b6a41c9923dbb4a1767d717a03
邬松 <1668330903@qq.com>
2023-01-07 22:33:15 +0800

    feat(v1.0.0): 添加企业微信机器人通知自动装配

---
+++ src/main/java/com/ws/an/config/abnormalnotice/AbnormalNoticeWechatNoticeAutoConfig.java
@@ -0,0 +1,19 @@
+package com.ws.an.config.abnormalnotice;
+
+import com.ws.an.properties.notice.EmailNoticeProperty;
+import com.ws.an.properties.notice.WechatNoticeProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author WuSong
+ * @version 1.0
+ * @date 2023/1/7 22:06
+ * @description 企业微信机器人通知自动装配
+ */
+@Configuration
+@ConditionalOnProperty(value = "abnormal.notice.wechat.enabled", havingValue = "true")
+@EnableConfigurationProperties({ WechatNoticeProperty.class })
+public class AbnormalNoticeWechatNoticeAutoConfig {
+}
---
+++ src/main/java/com/ws/an/properties/notice/WechatNoticeProperty.java
@@ -0,0 +1,13 @@
+package com.ws.an.properties.notice;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author WuSong
+ * @version 1.0
+ * @date 2023/1/7 22:15
+ * @description
+ */
+@ConfigurationProperties(prefix = "abnormal.notice.wechat")
+public class WechatNoticeProperty {
+}
